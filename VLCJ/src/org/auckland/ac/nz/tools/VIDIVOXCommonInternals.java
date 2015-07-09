6f3ab01850fbc466da1aa05115b6273c6a70115b
ebag753 <ebag753@xen-rlimage.ece.auckland.ac.nz>
2015-07-09 17:04:18 +1200

    Added Edit funcationality

---
+++ VLCJ/src/org/auckland/ac/nz/tools/VIDIVOXCommonInternals.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package org.auckland.ac.nz.tools;
+
+/**
+ * @author ebag753
+ *
+ */
+public final class VIDIVOXCommonInternals {
+    
+    /* This defines the items in our menu */
+    public enum MenuItems {
+        OPEN,
+        SAVE,
+        EXIT,
+        PLAY,
+        PAUSE,
+        RESUME,
+        STOP,
+        EDIT
+    };
+
+    public final static String YES_MSG = "Yes";
+    public final static String NO_MSG = "No";
+    
+    public final static String EXIT_APP = "Exit Application";
+    public final static String EXIT_NOTIFICATION = "Are you sure you want to exit from the application?";
+    
+    public final static String MENU_FILE = "File";
+    public final static String MENU_FILE_OPEN = "Open";
+    public final static String MENU_FILE_SAVE = "Save";
+    public final static String MENU_FILE_EXIT = "Exit";
+    
+    public final static String MENU_ACTIONS = "Actions";
+    public final static String MENU_ACTIONS_PLAY = "Play";
+    public final static String MENU_ACTIONS_PAUSE = "Pause";
+    public final static String MENU_ACTIONS_RESUME = "Resume";
+    public final static String MENU_ACTIONS_STOP = "Stop";
+    
+    public final static String MENU_EDIT = "Edit";
+    
+}
